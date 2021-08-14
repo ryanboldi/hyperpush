8670d5de8e66c5c03407d95e80cd097b813ef90c
Ryan Boldi <ryan.boldi123@gmail.com>
2021-08-14 14:28:03 -0400

    get output completed but doesn't work

--- src/hyperpush/cppn/core.clj
+++ src/hyperpush/cppn/core.clj
@@ -1,7 +1,9 @@
 (ns hyperpush.cppn.core
-  (:require [propeller.genome :as genome]
-            [propeller.push.interpreter :as interpreter]
-            [propeller.push.state :as state]))
+  (:require 
+   [hyperpush.cppn.utils :as utils]
+   [propeller.genome :as genome]
+    [propeller.push.interpreter :as interpreter]
+    [propeller.push.state :as state]))
 
 (defn get-input-map
   "returns map of key to input value like {:in# %# ...}"
@@ -10,12 +12,16 @@
        (map-indexed (fn [idx itm] (hash-map (keyword (str "in" (inc idx))) itm)))
        (into {})))
 
-(get-input-map [0.1 1.3 4.1 4.0])
+(defn get-output
+  "given a push program and a list of inputs, return the value at the top of float stack"
+  [push & inputs]
+  (let [output (state/peek-stack
+                (interpreter/interpret-program
+                 push
+                 (assoc state/empty-state :input (get-input-map inputs))
+                 100)
+                :float)
+        float-output (if (float? output) output 0)]
+        float-output))
 
-;(defn get-output
- ;"given a push program and a list of inputs, return the value at the top of float stack"
- ; [push & inputs]
- ; (let [output (state/peek-stack
-  ;              (interpreter/interpret-program
-   ;              push
-   ;              (assoc state/empty-state :input )))]))
+(get-output (genome/plushy->push (utils/random-plushy)) [0.1 0.3 0.4 0.5])
