e06260ee607016bdf73139a4b7ed5def649aacdd
Ryan Boldi <ryan.boldi123@gmail.com>
2021-08-17 11:52:24 -0400

    ensure substrate is vector

--- src/hyperpush/nn/substrate.clj
+++ src/hyperpush/nn/substrate.clj
@@ -1,11 +1,14 @@
 (ns hyperpush.nn.substrate)
 
+(defn convert-to-vector [substrate-layer]
+  (mapv vec substrate-layer))
+
 (defn make-substrate-layer
   "creates a single substrate layer of size width x height"
   [width height]
-  (if (or (<= width 0) (<= height 0))
+  (convert-to-vector (if (or (<= width 0) (<= height 0))
     '()
-    (repeatedly width #(repeat height 0))))
+    (repeatedly width #(repeat height 0)))))
 
 (defn get-width 
   "returns the width of a substrate layer"
