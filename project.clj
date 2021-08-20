26e1e11f0f84dce7689ee70b6623d2ffdcd1841e
Ryan Boldi <ryan.boldi123@gmail.com>
2021-08-20 13:29:24 -0400

    added matrix lib

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [net.clojars.lspector/propeller "0.2.3"]]
+                 [net.clojars.lspector/propeller "0.2.3"]
+                 [net.mikera/core.matrix "0.62.0"]]
   :main ^:skip-aot hyperpush.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
--- src/hyperpush/examples.clj
+++ src/hyperpush/examples.clj
@@ -4,15 +4,4 @@
             [hyperpush.cppn.core :as cppn]
             [hyperpush.cppn.utils :as utils]))
 
-
-(def input-layer (subs/make-2d-square-layer 2 1))
-(def output-layer (subs/make-2d-square-layer 1 1))
-
-(def connection-matrix (nn/create-network input-layer output-layer (utils/random-push)))
-
-connection-matrix
-
-(nn/feed-forward [[1] [1]] connection-matrix)
-
-(defn run-xor-problem []
-  ())
+(def nice 1)
