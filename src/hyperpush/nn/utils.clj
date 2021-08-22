aad9d2a441107f52f66047620e36ecbef934955d
Ryan Boldi <ryan.boldi123@gmail.com>
2021-08-22 12:14:27 -0400

    Update utils.clj

--- src/hyperpush/nn/utils.clj
+++ src/hyperpush/nn/utils.clj
@@ -3,7 +3,7 @@
 (defn convert-to-high-d-vector [substrate-layer]
   (mapv vec substrate-layer))
 
-(defn normalize-center-biased 
+(defn normalize-center-biased
   "normalizes the index of a node from 0 to 1, given the num of nodes in the layer, center biased"
   [index num]
   (let [gap (/ 1 (inc num))
